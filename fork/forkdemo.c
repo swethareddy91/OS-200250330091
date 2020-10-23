3461f66270da01d7f6b3248469386d4698987e66
swetha <sareddyswethaa@gmail.com>
2020-10-23 12:56:07 -0400

    forkdemo

---
+++ fork/forkdemo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/types.h>
+ #include <unistd.h>
+
+int main(int argc,char const *argv[])
+{
+     pid_t id;
+
+     printf("Before fork\n");
+     id = fork();
+     printf("After fork\n");
+     return 0;
+     
+}
