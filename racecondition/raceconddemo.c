d39cf84485a3b2de228767f81f7bced38ff37da9
swetha <sareddyswethaa@gmail.com>
2020-10-30 12:39:25 -0400

    INC and DEC THREAD demo

--- racecondition/raceconddemo.c
+++ racecondition/raceconddemo.c
@@ -5,30 +5,35 @@
 int count = 0;
 void *inc_thread(void *arg)
 {
-    while (1)
+    while(1)
     {
-        count++;
-        printf("Inc Thread : %d\n",count);
 
+        count++;
+        printf("INC Thread: %d\n",count);
     }
 }
+
 void *dec_thread(void *arg)
 {
-    while (1)
+    while(1)
     {
-        count --;
-        printf("Dec Thread: %d\n",count);
-    }
-}
- 
- int main(int argc,char const *argv[])
-{
 
+        count--;
+        printf("DEC Thread: %d\n",count);
+    }
+
+}
+
+int main()
+{
     pthread_t tid_inc, tid_dec;
+    printf("Before thread creation\n");
     pthread_create(&tid_inc, NULL, inc_thread, NULL);
     pthread_create(&tid_dec, NULL, dec_thread, NULL);
-    pthread_join(tid_inc, NULL);
-    pthread_join(tid_dec, NULL); 
 
+    pthread_join(tid_inc, NULL);
+    pthread_join(tid_dec, NULL);
     return 0;
+
 }
+    
